54f433ce4dd6c34ee756e6ee25911b3d36534032
pola0noch <klayriddell@gmail.com>
2024-12-23 22:28:48 +0300

    version.1

--- main.py
+++ main.py
@@ -9,32 +9,32 @@ class User(BaseModel):
     age: int
 
 class UserCreate(BaseModel):
-    id: int = Field(gt=0,
-                    description='User id from int in list')
-    # noinspection PyArgumentList
-    username: str = Field(min_lenght=3,
-                          max_lenght=20)
+    id: int = Field(description='User id from int in list')
+
+    username: str = Field(min_length=3,
+                          max_length=20,
+                          description='Username must be between 3 and 20 characters long')
     age: int = Field(gt=0,
                      le=150,
                      description='Age from 0 to 150')
 
 app = FastAPI()
 
-users: List[User] = [User(id=0, username='Имя пользователя', age=0)]
+users: List[User] = []
 
 @app.get("/users", response_model=List[User])
-async def get_users() -> List:
+async def get_users() -> List[User]:
     return users
 
 @app.post("/user/{username}/{age}", response_model=User)
-async def register_user(user: UserCreate) -> User:
+async def register_user(user: UserCreate):
     new_user_id = max((u.id for u in users), default=0) + 1
     new_user = User(id=new_user_id, username=user.username, age=user.age)
     users.append(new_user)
     return new_user
 
 @app.put("/user/{user_id}/{username}/{age}", response_model=User)
-async def update_user(user_id: int, user: UserCreate) -> User:
+async def update_user(user_id: int, user: UserCreate):
     for u in users:
         if u.id == user_id:
             u.username = user.username
@@ -43,11 +43,11 @@ async def update_user(user_id: int, user: UserCreate) -> User:
     raise HTTPException(status_code=404, detail="User not found")
 
 @app.delete("/user/{user_id}", response_model=dict)
-async def delete_user(user_id: int) -> User:
+async def delete_user(user_id: int):
     for i, u in enumerate(users):
         if u.id == user_id:
             del users[i]
-            return u
+            return {"detail": f"User {u.username} with id {user_id} has been deleted"}
     raise HTTPException(status_code=404, detail="User not found")
 
 
